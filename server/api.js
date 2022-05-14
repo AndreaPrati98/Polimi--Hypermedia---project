59cb57bafb9066084ded7c82b58e9e8cc0145a01
Andrea Prati <a.prati@outlook.it>
2022-05-14 15:43:50 +0200

    again, new tries

--- server/api.js
+++ server/api.js
@@ -4,14 +4,14 @@ const { Sequelize, DataTypes } = require("sequelize")
 const initialize = require('./initialize').default
 app.use(express.json())
 
-// const database = new Sequelize("postgres://postgres:postgres@localhost:5432/hyp")
+const database = new Sequelize("postgres://postgres:postgres@localhost:5432/hyp")
 // Production
-const pgVar = require( 'pg' )
-pgVar.defaults.ssl = true
-const database = new Sequelize(process.env.DATABASE_URL, {
-    ssl: true,
-    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, 
-    })
+// const pgVar = require( 'pg' )
+// pgVar.defaults.ssl = true
+// const database = new Sequelize(process.env.DATABASE_URL, {
+//     ssl: true,
+//     dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, 
+//     })
 
 
 // Function that will initialize the connection to the database
