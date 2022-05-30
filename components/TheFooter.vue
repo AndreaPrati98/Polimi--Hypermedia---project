81755258258b5e5f2df8aebfa7423d3b6a9dab56
dmora4 <destiny.mora@hotmail.com>
2022-05-30 16:23:53 +0200

    adding basic footer

---
+++ components/TheFooter.vue
@@ -0,0 +1,47 @@
+<template>
+ 
+<div class="container">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+    </ul>
+    <p class="text-center text-muted">&copy; 2022 Company, Inc</p>
+  </footer>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+  // data() {
+  //   return {
+  //     headerList: [
+  //       {
+  //         name: 'Home',
+  //         path: '/',
+  //       },
+  //       {
+  //         name: 'Town',
+  //         path: '/',
+  //       },
+  //       {
+  //         name: 'Things To Do',
+  //         path: '/',
+  //       },
+  //       {
+  //         name: 'Services',
+  //         path: '/',
+  //       },
+  //       {
+  //         name: 'Contact Us',
+  //         path: '/',
+  //       },
+  //     ],
+  //   }
+  // },
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,18 @@
   <div class="main-page">
     <the-header />
     <nuxt />
+    <the-footer/>
   </div>
 </template>
 
 <script>
+import TheFooter from '~/components/TheFooter.vue'
 import TheHeader from '~/components/TheHeader.vue'
 export default {
   name: 'DefaultLayout',
   components: {
     TheHeader,
+    TheFooter,
   },
 }
 </script>
