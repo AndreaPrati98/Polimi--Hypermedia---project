82e3b03abd26facbfabd441140cfc6973bbd79b6
Andrea Prati <a.prati@outlook.it>
2022-05-30 17:06:38 +0200

    added nav items with vue logic

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -19,49 +19,17 @@
       </button>
       <div id="navbarSupportedContent" class="collapse navbar-collapse">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
-            <nuxt-link class="nav-link active" aria-current="page" to="/">Home</nuxt-link>
-          </li>
-          <li class="nav-item">
-            <nuxt-link class="nav-link" to="/secondPage">Link page</nuxt-link>
-          </li>
-          <li class="nav-item dropdown">
-            <nuxt-link
-              id="navbarDropdown"
-              class="nav-link dropdown-toggle"
-              to="/"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Dropdown
+          <li
+          v-for="(navItem, navItemIndex) of headerList"
+          :key="`navItem${navItemIndex}`"
+          class="nav-item"
+          >  
+            <nuxt-link :to="navItem.path" class="nav-link">
+              {{ navItem.name }}
             </nuxt-link>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><nuxt-link class="dropdown-item" to="/">Action</nuxt-link></li>
-              <li><nuxt-link class="dropdown-item" to="/">Another action</nuxt-link></li>
-              <li><hr class="dropdown-divider" /></li>
-              <li><nuxt-link class="dropdown-item" to="/">Something else here</nuxt-link></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <nuxt-link
-              class="nav-link disabled"
-              to="/"
-              tabindex="-1"
-              aria-disabled="true"
-              >Disabled</nuxt-link>
           </li>
         </ul>
-        <form class="d-flex">
-          <input
-            class="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        
       </div>
     </div>
   </nav>
@@ -70,31 +38,31 @@
 <script>
 export default {
   name: 'TheHeader',
-  // data() {
-  //   return {
-  //     headerList: [
-  //       {
-  //         name: 'Home',
-  //         path: '/',
-  //       },
-  //       {
-  //         name: 'Town',
-  //         path: '/',
-  //       },
-  //       {
-  //         name: 'Things To Do',
-  //         path: '/',
-  //       },
-  //       {
-  //         name: 'Services',
-  //         path: '/',
-  //       },
-  //       {
-  //         name: 'Contact Us',
-  //         path: '/',
-  //       },
-  //     ],
-  //   }
-  // },
+  data() {
+    return {
+      headerList: [
+        {
+          name: 'Home',
+          path: '/',
+        },
+        {
+          name: 'Town',
+          path: '/',
+        },
+        {
+          name: 'Things To Do',
+          path: '/',
+        },
+        {
+          name: 'Services',
+          path: '/',
+        },
+        {
+          name: 'Contact Us',
+          path: '/',
+        },
+      ],
+    }
+  },
 }
 </script>
