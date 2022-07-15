effd6144f2d4f98d86a78866347f1ddc13e45784
Andrea Prati <a.prati@outlook.it>
2022-07-15 18:04:57 +0200

    removed eslint and prettier. Fixed the Vuex version for Vue2

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,14 +42,13 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    '@nuxt/content'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
