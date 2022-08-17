10cfcd302aff09fe378bb4e33a3b9fadc061a4bb
Andrea Prati <a.prati@outlook.it>
2022-08-17 15:16:10 +0200

    added a first version of the overview

---
+++ components/descriptive-component.vue
@@ -0,0 +1,46 @@
+<template>
+    <section class="section">
+        <div class="overview-title">
+            <h2>
+                Overview
+            </h2>
+        </div>
+        <div class="overview-paragraph">
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt quae facere amet voluptate asperiores fuga explicabo. Corrupti consectetur doloremque nostrum temporibus animi harum rem soluta repellendus, excepturi, odio aspernatur modi ratione nobis! Architecto beatae odio blanditiis asperiores laborum veritatis perspiciatis possimus facere. Deserunt aliquid quas id odit dignissimos at voluptas dolor, harum facilis, assumenda eligendi quibusdam. Praesentium unde veniam non hic sapiente neque incidunt maxime eveniet, excepturi optio vero deserunt! Explicabo qui, provident soluta ut libero repudiandae inventore, dolores rerum accusamus atque harum quaerat aspernatur officiis! Quia, fuga rem. Libero eum, hic vitae deserunt explicabo ducimus quaerat magni labore debitis?
+            </p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "descriptive-component",
+}
+</script>
+
+<style scoped>
+    .section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        
+        margin-top: 20px;
+        border: 1px solid red;
+    }
+    
+    .overview-title {
+        position: relative;
+        top: 35px;
+        right: 100px;
+        border: 1px solid red;
+        padding: 6px 20px;
+    }
+
+    .overview-paragraph {
+        max-width: 65ch;
+        padding: 30px;
+        border: 1px solid blue;
+    }
+
+</style>
--- pages/festival-intro.vue
+++ pages/festival-intro.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <title-overlay-image class="title-overlay-image"/>
+    <DescriptiveComponent class="descriptive-component"/>
   </div>
 </template>
 
 <script>
-import titleOverlayImage from '~/components/title-overlay-image.vue'
+import TitleOverlayImage from '~/components/title-overlay-image';
+import DescriptiveComponent from '~/components/Descriptive-component';
+
 export default {
-  components: { titleOverlayImage },
+  components: { TitleOverlayImage, DescriptiveComponent },
     name: "festival-intro",
 }
 </script>
-
-<style>
-</style>
