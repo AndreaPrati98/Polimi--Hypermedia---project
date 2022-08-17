7db45e84de40ffc0e04cf06d5a64f344f55edc4c
Andrea Prati <a.prati@outlook.it>
2022-08-17 11:19:05 +0200

    created page festival-intro. Implemented main img

---
+++ pages/festival-intro.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <title-overlay-image class="title-overlay-image"/>
+  </div>
+</template>
+
+<script>
+import titleOverlayImage from '~/components/title-overlay-image.vue'
+export default {
+  components: { titleOverlayImage },
+    name: "festival-intro",
+}
+</script>
+
+<style>
+</style>
