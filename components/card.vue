7cfed7fef8d20977069283e68c9ebb96122580d0
Andrea Prati <a.prati@outlook.it>
2022-08-17 16:16:23 +0200

    implementation card (for history) NOT USABLE YET

---
+++ components/card.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="section">
+    <div class="div-container">
+        <div class="img">
+
+        </div>
+        <div class="text-container">
+            <div class="title">
+                <h2>
+                    History
+                </h2>
+            </div>
+            <p class="paragraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem obcaecati in facere dolores. Culpa, illo. Natus debitis optio et voluptatum delectus magni quos nam perspiciatis dolores at, dolorum tempore aut!
+            </p>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+    .section {
+        border: 1px solid red;
+        width: 35%;
+        height: 40vh;
+        margin: 30px 0;
+    }
+
+    .div-container {
+        /* useful to use the % also with the children */
+        height: 100%;  
+    }
+
+    .img {
+        border: 2px solid red;
+        height: 80%;   
+    }
+
+    .text-container {
+        border: 2px solid purple;
+        height: 100%;
+    }
+
+    .title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 20%;
+        font-size: 1.7rem;
+        
+    }
+
+    .paragraph {
+        
+    }
+
+
+</style>
--- pages/festival-intro.vue
+++ pages/festival-intro.vue
@@ -2,15 +2,28 @@
   <div>
     <title-overlay-image class="title-overlay-image"/>
     <DescriptiveComponent class="descriptive-component"/>
+    <div class="card-container">
+      <Card class="card"/>
+    </div>
   </div>
 </template>
 
 <script>
 import TitleOverlayImage from '~/components/title-overlay-image';
 import DescriptiveComponent from '~/components/Descriptive-component';
+import Card from '~/components/card.vue';
 
 export default {
-  components: { TitleOverlayImage, DescriptiveComponent },
+  components: { TitleOverlayImage, DescriptiveComponent, Card },
     name: "festival-intro",
 }
 </script>
+
+<style scoped>
+
+  .card-container{
+    display: flex;
+    justify-content: center;
+  }
+
+</style>
