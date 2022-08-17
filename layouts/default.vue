06ea695d9eeddeb2ab1bdbfaef965696d4fe3b09
Andrea Prati <a.prati@outlook.it>
2022-08-17 15:15:52 +0200

    mino change

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="main-page">
-    <new-header/>
+    <NewHeader/>
     <nuxt />
   </div>
 </template>
 
 <script>
-import NewHeader from '~/components/new-header.vue'
-// import TheHeader from '~/components/TheHeader.vue'
+import NewHeader from '~/components/new-header.vue';
+
+
 export default {
   name: 'DefaultLayout',
   components: {
     NewHeader,
-    // TheHeader,
-
   },
 }
 </script>
