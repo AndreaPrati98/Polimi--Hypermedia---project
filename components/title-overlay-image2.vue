708540e95527b8114d9f9636280bcc088bb53b2a
Andrea Prati <a.prati@outlook.it>
2022-08-21 19:14:19 +0200

    used the new card into a component, and so in the page

--- components/title-overlay-image2.vue
+++ components/title-overlay-image2.vue
@@ -1,39 +1,111 @@
 <template>
-    <section class="section">
-        <div class="info">
-            <h1>Title</h1>
-            <h4>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt quod facere porro
-                repellendus distinctio, iusto doloribus dicta, aspernatur error optio deleniti beatae. 
-                Officiis in consequuntur sit, quisquam quibusdam nulla veniam?
-            </h4>
-        </div>
-        <div class="imgs">
-            <card-hover-b-w 
-                :descTxt="'Go to ticket section'"
-                :btnTxt="'Tickets'"/>
-            
-            <div class="img2">
-                Img2
+    <section class="container">
+        <section class="section">
+            <div class="info">
+                <h1>Title</h1>
+                <h4>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt quod facere porro
+                    repellendus distinctio, iusto doloribus dicta, aspernatur error optio deleniti beatae. 
+                    Officiis in consequuntur sit, quisquam quibusdam nulla veniam?
+                </h4>
             </div>
-            
-            <div class="img3">
-                Img3
-            </div>
+    
+            <div class="imgs">
+                <card-hover-b-w 
+                    :descTxt="this.resultObj.tickets.description"
+                    :btnTxt="'Tickets'"
+                    :imgUrl="this.resultObj.tickets.imgUrl"/>
                 
-            <div class="img4">
-                img4
+                <card-hover-b-w 
+                    :descTxt= "this.resultObj.date.description"
+                    :btnTxt= "this.resultObj.date.buttonTxt"
+                    :imgUrl="this.resultObj.date.imgUrl"
+                    :btnDst="this.resultObj.date.buttonDst"/>
+                
+                <card-hover-b-w 
+                    :descTxt= "this.resultObj.accessibility.description"
+                    :btnTxt= "this.resultObj.accessibility.buttonTxt"
+                    :imgUrl="this.resultObj.accessibility.imgUrl"
+                    :btnDst="this.resultObj.accessibility.buttonDst"/>
+    
+                <card-hover-b-w 
+                    :descTxt= "this.resultObj.location.description"
+                    :btnTxt= "this.resultObj.location.buttonTxt"
+                    :imgUrl="this.resultObj.location.imgUrl"
+                    :btnDst="this.resultObj.location.buttonDst"/>
+    
             </div>
+        </section>
 
-        </div>
+        <section id="ticket">
+            Ticket section
+        </section>
+
+        <section id="date">
+            date section
+        </section>
+
+        <section id="Location">
+            location section
+        </section>
+
+        <section id="accessibility">
+            accessibility section
+        </section>
     </section>
 </template>
 
 <script>
 import cardHoverBW from './img-component/card-hover-b-w.vue'
+import CardHoverBW from './img-component/card-hover-b-w.vue';
 export default {
-  components: { cardHoverBW },
     name: "festival-practical-info",
+    components: { cardHoverBW, CardHoverBW },
+    data() {
+        // here we will use axios to retrieve content from the db
+        var pageTxtList = {
+            tickets: {
+                description: "Go to tickets",
+                buttonTxt: "Tickets",
+                buttonDst: "#date",
+                imgUrl: "https://cdn.pixabay.com/photo/2017/11/24/10/43/ticket-2974645_1280.jpg",
+            },
+            date: {
+                description: "Go to date",
+                buttonTxt: "Date",
+                buttonDst: "#date",
+                imgUrl: "https://cdn.pixabay.com/photo/2017/01/18/17/52/calendar-1990453_1280.jpg",
+            },
+            location: {
+                description: "Go to location",
+                buttonTxt: "Location",
+                buttonDst: "#location",
+                imgUrl: "https://cdn.pixabay.com/photo/2018/08/01/21/26/map-3578213_1280.jpg",
+            },
+            accessibility: {
+                description: "Go to accessibility",
+                buttonTxt: "accessibility",
+                buttonDst: "#accessibility",
+                imgUrl: "https://cdn.pixabay.com/photo/2015/11/20/18/57/ampel-pusher-1053727_1280.jpg",
+            },  
+        }
+        // this return the number of entries 
+        const count = Object.keys(pageTxtList).length; 
+        var resultObj
+        // in case we consider more than 4 elements, the system returns only 4 of them
+        if(count > 4) {
+            var entries;
+            entries = Object.keys(pageTxtList);
+            for(const element of entries) {
+                resultObj[element] = pageTxtList[element]
+            }
+            console.log("Someone inserted more than 4 elements for the card content");
+            return resultObj
+        } else {
+            resultObj = pageTxtList
+        }
+        return { resultObj }
+    }
 }
 </script>
 
@@ -63,10 +135,7 @@ export default {
         
     }
 
-    .imgs div:hover {
-        filter: grayscale(100%);
-        opacity: .7;
-    }
+    
 
     .img1 {
         background-image: url(../assets/imgs/data.png);
