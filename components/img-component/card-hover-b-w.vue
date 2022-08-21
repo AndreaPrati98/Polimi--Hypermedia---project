ddd453a856ad8ca121df1dfc2df048cbd93653db
Andrea Prati <a.prati@outlook.it>
2022-08-21 12:47:05 +0200

    first usable implementation of the car hover black and white

--- components/img-component/card-hover-b-w.vue
+++ components/img-component/card-hover-b-w.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="img">
-        <div class="txt">
-            <h2>Titolo</h2>
-            <p>Piccola descrizione</p>
-            <button-std class="btn" :btnDst="'/festival-practical-info'"/>
+        <div class="txt-wrapper">
+            <div class="txt">
+                <h2>Titolo</h2>
+                <!-- <p>Piccola descrizione</p> -->
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste minima quo consequuntur nulla laborum inventore? Quam deleniti esse ducimus eveniet!</p>
+                <button-std class="btn" :btnDst="'/festival-practical-info'"/>
+            </div>
         </div>
     </div>
 </template>
@@ -28,20 +31,38 @@ export default {
         
     }
 
-    .txt {
-        border: 2px solid green;
-        color: rgba(0, 0, 0, 1) !important;
+    .txt-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
         height: 100%;
+
+        border: 2px solid green;
         
         transition: backdrop-filter .3s ease-in .2s,
                     background-color .3s ease-out,
                         
     }
 
-    .txt:hover {
+    .txt-wrapper:hover {
         backdrop-filter: grayscale(100%);
         background-color: rgba(255, 255, 255, 0.3);
     }
 
+    .txt {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        overflow: scroll;
+
+        padding: 10px;
+        border: 1.5px solid black;
+        
+        /* backdrop-filter: opacity(1); */
+        background-color: rgba(255, 255, 255, 0.8);
+    }
 
 </style>
--- pages/festival-practical-info.vue
+++ pages/festival-practical-info.vue
@@ -1,8 +1,15 @@
 <template>
     <div class="container">
         <TitleOverlayImage2/>
-        <div class="btn-div">
-            <button-std-animated :btnDst="'/festival-practical-info'" :btnBkgdColor="'yellow'" :btnTextColor="'red'" :btnBkgdHoverColor="'green'"/>
+        <div class="inner-container">
+            <span class="separator"></span>
+            <div class="card-container">
+                <CardHoverBW class="cardHoverBW"/>
+            </div> 
+            <span class="separator"></span>
+            <div class="btn-container">
+                <button-std-animated :btnDst="'/festival-practical-info'" :btnBkgdColor="'yellow'" :btnTextColor="'red'" :btnBkgdHoverColor="'green'"/>
+            </div>
         </div>
     </div>
 </template>
@@ -10,17 +17,32 @@
 <script>
 import ButtonStdAnimated from '~/components/utilities-components/button-std-animated.vue';
 import TitleOverlayImage2 from "../components/title-overlay-image2.vue";
+import CardHoverBW from "../components/img-component/card-hover-b-w.vue";
 export default {
     name: "festival-practical-info",
-    components: { TitleOverlayImage2, ButtonStdAnimated }
+    components: { TitleOverlayImage2, ButtonStdAnimated, CardHoverBW }
 }
 </script>
 
 <style scoped>
 
-    .btn-div {
+    .separator{
+        width: 100%;
+        height: 20px;
+    }
+    .inner-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-content: center;
+    }
+    .btn-container, .card-container {
         display: flex;
         justify-content: center;
+    }
+    .cardHoverBW {
+        height: 70vh;
+        width: 50%;
     }
 
 </style>    
