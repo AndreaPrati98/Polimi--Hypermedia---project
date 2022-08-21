764f32b31551c7f103c1ed6e1848d74fddd4ec09
Andrea Prati <a.prati@outlook.it>
2022-08-21 09:52:47 +0200

    added example on how to use the animated btn

--- pages/festival-practical-info.vue
+++ pages/festival-practical-info.vue
@@ -1,14 +1,26 @@
 <template>
-    <TitleOverlayImage2/>
+    <div class="container">
+        <TitleOverlayImage2/>
+        <div class="btn-div">
+            <button-std-animated :btnDst="'/festival-practical-info'" :btnBkgdColor="'yellow'" :btnTextColor="'red'" :btnBkgdHoverColor="'green'"/>
+        </div>
+    </div>
 </template>
 
 <script>
+import ButtonStdAnimated from '~/components/utilities-components/button-std-animated.vue';
 import TitleOverlayImage2 from "../components/title-overlay-image2.vue";
 export default {
     name: "festival-practical-info",
-    components: { TitleOverlayImage2 }
+    components: { TitleOverlayImage2, ButtonStdAnimated }
 }
 </script>
 
 <style scoped>
+
+    .btn-div {
+        display: flex;
+        justify-content: center;
+    }
+
 </style>    
