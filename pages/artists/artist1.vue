f3ff12b38cb45a9543c1233e8c74f0e7becfb0dc
dmora4 <destiny.mora@hotmail.com>
2022-08-22 04:26:46 +0200

    adding the section to add the links to events TODO: make a carousel

--- pages/artists/artist1.vue
+++ pages/artists/artist1.vue
@@ -1,26 +1,40 @@
 <template>
   <section>
-    <div class="bar">
-        <h1>Name of Artist</h1>
-        <div>
-            <ul>
-                <li>Jazz musician</li>
-                <li>Active since 2014</li>
-                <li>Seattle, WA</li>
-            </ul>
+    <div>
+        <div class="bar">
+            <h1>Name of Artist</h1>
+            <div>
+                <ul>
+                    <li>Jazz musician</li>
+                    <li>Active since 2014</li>
+                    <li>Seattle, WA</li>
+                </ul>
+            </div>
         </div>
+        <div class="left-slot"></div>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dictum augue in lectus maximus volutpat. Ut ac libero pulvinar, dignissim ex in, placerat nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; </p>
+        <div class="picure1">
+            <img class="picture left" src="~/assets/imgs/artists/samples/sydney.jpg"/>
+        </div>
+        <p>Nullam at pharetra nisl. Vivamus lobortis elit in nisl feugiat, ac dapibus nibh vehicula. Fusce vel risus eu ante ultricies rutrum et id arcu. Ut massa tellus, consectetur non neque nec, fermentum consectetur risus. Ut ut venenatis diam, sit amet suscipit nunc. Vivamus sem urna, pellentesque non justo vel, faucibus aliquam sem.Cras lacus eros, ullamcorper suscipit suscipit at, eleifend et mauris. Suspendisse eleifend ante sed lacus ultrices lobortis. Etiam vitae massa eros. Cras vestibulum facilisis suscipit. Sed venenatis nisl non ante volutpat, vitae porta quam faucibus. Nam ultrices, enim nec aliquam iaculis, eros mi porta turpis, et fringilla augue est non nulla. Nam id quam a ligula accumsan elementum.Praesent dictum purus vitae nunc eleifend sodales. Nulla dignissim arcu eu arcu lacinia, in dapibus tellus tempus.</p>
+        <div class="picture2">
+            <img class="picture right" src="~/assets/imgs/artists/samples/violin.jpg"/>
+        </div>
+        <p> Praesent ipsum diam, scelerisque eu maximus vel, semper vitae urna. Nam egestas ac justo nec condimentum. Praesent egestas at mauris at pharetra. Nulla id risus nec tortor facilisis faucibus. Duis feugiat viverra mauris, eget blandit libero.</p>
     </div>
-    <div class="left-slot"></div>
-    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dictum augue in lectus maximus volutpat. Ut ac libero pulvinar, dignissim ex in, placerat nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; </p>
-    <div class="picure1">
-        <img class="picture left" src="~/assets/imgs/artists/samples/sydney.jpg"/>
-    </div>
-    <p>Nullam at pharetra nisl. Vivamus lobortis elit in nisl feugiat, ac dapibus nibh vehicula. Fusce vel risus eu ante ultricies rutrum et id arcu. Ut massa tellus, consectetur non neque nec, fermentum consectetur risus. Ut ut venenatis diam, sit amet suscipit nunc. Vivamus sem urna, pellentesque non justo vel, faucibus aliquam sem.Cras lacus eros, ullamcorper suscipit suscipit at, eleifend et mauris. Suspendisse eleifend ante sed lacus ultrices lobortis. Etiam vitae massa eros. Cras vestibulum facilisis suscipit. Sed venenatis nisl non ante volutpat, vitae porta quam faucibus. Nam ultrices, enim nec aliquam iaculis, eros mi porta turpis, et fringilla augue est non nulla. Nam id quam a ligula accumsan elementum.Praesent dictum purus vitae nunc eleifend sodales. Nulla dignissim arcu eu arcu lacinia, in dapibus tellus tempus.</p>
-    <div class="picture2">
-        <img class="picture right" src="~/assets/imgs/artists/samples/violin.jpg"/>
-    </div>
-    <p> Praesent ipsum diam, scelerisque eu maximus vel, semper vitae urna. Nam egestas ac justo nec condimentum. Praesent egestas at mauris at pharetra. Nulla id risus nec tortor facilisis faucibus. Duis feugiat viverra mauris, eget blandit libero.</p>
     
+    
+   
+
+    <hr/>
+    <div class="where">
+        <h3>Events including this artist:</h3>
+    </div>
+    
+
+    
+
+
 
     <footer />
   </section>
@@ -99,6 +113,28 @@ img {
     margin-right: 2%;
     }
 
+.where {
+    display:flex;
+    position: relative;
+}
+hr {
+    width:70%;
+    margin-left: auto;
+    margin-right: auto;
+    color: red;
+    margin-top: 30px;
+    
+}
+section {
+    display: flex;
+    flex-wrap: wrap;
+}
 
+.where {
+    width:100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+}
 
 </style>
