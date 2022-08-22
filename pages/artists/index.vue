6ad606c0e4b418c61d60146ee4aa9903c5473f49
dmora4 <destiny.mora@hotmail.com>
2022-08-22 17:12:23 +0200

    changed the index2 filename to index [for all artists page]

--- pages/artists/index.vue
+++ pages/artists/index.vue
@@ -1,20 +1,36 @@
 <template>
   <section>
     <title-overlay-image class="title-overlay-image-props" title="This Year's Artists" description="Check this out!" image="palais-des-papes-g04269230e_1920.jpg"/>
-    <div class="flex-container">
-        <div class="title-box dance">
-          <h1>Dance</h1>
+    <div class="row">
+      <div class="column dance">
+        <div class="item title-box">
+          <h2>Dance</h2>
         </div>
-        <div class="title-box music">
-          <h1>Music</h1>
+        <card-hover icon artistName="Test1" imageName='dance1.png'/>
+        <card-hover artistName="Test2" imageName='dance2.jpg'/>
+
+      </div>
+      <div class="column music">
+        <div class="item title-box">
+          <h2>Music</h2>
         </div>
-        <div class="title-box theater">
-          <h1>Theater</h1>
+        <card-hover artistName="Test3" imageName='music1.jpg'/>
+        <card-hover artistName="Test4" imageName='music2.jpg'/>
+        <card-hover artistName="More Music 1" imageName='music3.jpg'/>
+        <card-hover artistName="More Music 2" imageName='music4.jpg'/>
+        <card-hover artistName="More Music 3" imageName='music5.jpg'/>
+
+
+      </div>
+      <div class="column theater">
+        <div class="item title-box">
+          <h2>Theater</h2>
         </div>
-        <!-- TODO: loop through all artists -->
-        <artist-icon class = "dance" artistName="Dancer 1" imageName="dance2.jpg"/>
-        <!--artist-icon class = "music" artistName="Musician 1" imageName="music1.jpg"/-->
-  
+        
+        <card-hover artistName="Test5" imageName='theater1.jpg'/>
+        <card-hover artistName="Test6" imageName='theater2.jpg'/>
+
+      </div>
     </div>
     <footer />
   </section>
@@ -23,49 +39,46 @@
 <script>
 import TitleOverlayImage from '~/components/title-overlay-image.vue';
 import Footer from '~/components/footer.vue';
-import ArtistIcon from '~/components/artist-icon.vue';
+import CardHover from '~/components/img-component/card-simple-hover.vue';
 export default {
     name: "artists",
-    components: {TitleOverlayImage, ArtistIcon, Footer}
+    components: {TitleOverlayImage, CardHover, Footer}
 
 }
 </script>
 
 <style>
 
-.flex-container {
-  display: grid;
-  flex-direction: column;
-  grid-template-columns: repeat(3, minmax(0, 1fr));
-  /*grid-auto-flow: column;*/
-  justify-content: space-between;
-}
-
-/* Intention is to fill up the remainder of the bottom of the window with the title box when you 
- * first open a full-size window */
-.title-box{
+/* Set up the grid structure to list all the artists */
+.row {
   display: flex;
-  height: 25vh;
-  background-color: red;    /* Color of title box  */
-  color: white;             /* Color of text */
-  align-items: center;        /* Vertical centering of text*/
-  justify-content: center;    /* Horizontal centering of text */
+  flex-wrap: wrap;
 }
 
-.dance {
-  order: 1;
-  flex-direction: column;
-}
-.music {
-  order: 2;
-  flex-direction: column;
-
-}
-.theater {
-  order: 3;
-  flex-direction: column;
-
+.column {
+  flex: 33%;
+  justify-content: space-around; 
 }
 
+.column .item{
+  width: 100%;
+}
+
+/* Title of the column */
+.title-box {
+  background-color: red;
+  color: white;
+  height: 30vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+}
+
+h2 {
+  font-family: 'Poppins';
+  font-weight: 200;
+  font-size: 2.5em;
+}
 
 </style>
