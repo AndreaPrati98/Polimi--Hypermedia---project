9dc37540cee91c4ea375eb13619b40363415add9
Andrea Prati <a.prati@outlook.it>
2022-08-23 19:11:29 +0200

    updated festival-intro with the new header

--- pages/festival-intro.vue
+++ pages/festival-intro.vue
@@ -1,32 +1,37 @@
 <template>
   <div>
-    <title-overlay-image class="title-overlay-image"/>
+    <TitleOverlayImage2 class="header"/>
     <DescriptiveComponent class="descriptive-component"/>
     <div class="double-section">
       <Card class="card"/>
-      <InfoComponent class="info-component"/>
+      <InfoComponent :title="'Practical informations'" class="info-component"/>
     </div>
     <Map class="map"/>
   </div>
 </template>
 
 <script>
-import TitleOverlayImage from '~/components/title-overlay-image';
 import DescriptiveComponent from '~/components/Descriptive-component';
 import Card from '~/components/card.vue';
 import InfoComponent from '~/components/information-components/info-component.vue';
 import Map from '~/components/map.vue';
 import Footer from '../components/footer.vue';
 import ButtonStd from '~/components/utilities-components/button-std.vue';
+import TitleOverlayImage2 from '~/components/title-overlay-image2.vue';
 
 export default {
-  components: { TitleOverlayImage, DescriptiveComponent, Card, InfoComponent, Map, Footer, ButtonStd },
+  components: { DescriptiveComponent, Card, InfoComponent, Map, Footer, ButtonStd, TitleOverlayImage2 },
     name: "festival-intro",
   
 }
 </script>
 
 <style scoped>
+
+  .header {
+    height: 90vh;
+  }
+
   .double-section{
     display: flex;
     justify-content: space-around;
