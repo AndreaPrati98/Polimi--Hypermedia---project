c17e4fbd90664ed8d94858fa168cda183b0e50cf
dmora4 <destiny.mora@hotmail.com>
2022-08-23 09:28:15 +0200

    ported out the transitional links as a transitional-link component from the artists page

--- pages/artists/artist1.vue
+++ pages/artists/artist1.vue
@@ -16,36 +16,22 @@
         <p> Praesent ipsum diam, scelerisque eu maximus vel, semper vitae urna. Nam egestas ac justo nec condimentum. Praesent egestas at mauris at pharetra. Nulla id risus nec tortor facilisis faucibus. Duis feugiat viverra mauris, eget blandit libero.</p>
     </div>
     <hr/>
-    <div class="where">
-        <h3>Events including this artist:</h3>
-        <div class="scroll-window">
-            <div class="events">
 
-                <card-additional-content class="event" title="Event 1" content="Friday 22/06 10AM" :imgUrl="'https://cdn.pixabay.com/photo/2016/11/21/16/01/buildings-1846133_1280.jpg'" btnTxt="See More" btnDst="~/pages/events/event1"/>
-                <card-additional-content class="event" title="Event 2" content="Saturday 19/06 7PM" :imgUrl="'https://cdn.pixabay.com/photo/2016/11/21/16/01/buildings-1846133_1280.jpg'" btnTxt="See More" btnDst="~/pages/events/event1"/>
-                <card-additional-content class="event" title="Event 3" content="Saturday 19/06 19:30pm" :imgUrl="'https://cdn.pixabay.com/photo/2016/11/21/16/01/buildings-1846133_1280.jpg'" btnTxt="See More" btnDst="~/pages/events/event1"/>
-                <card-additional-content class="event" title="Event 4" content="Friday 22/06 10AM" :imgUrl="'https://cdn.pixabay.com/photo/2016/11/21/16/01/buildings-1846133_1280.jpg'" btnTxt="See More" btnDst="~/pages/events/event1"/>
-                <card-additional-content class="event" title="Event 5" content="Saturday 19/06 7PM" :imgUrl="'https://cdn.pixabay.com/photo/2016/11/21/16/01/buildings-1846133_1280.jpg'" btnTxt="See More" btnDst="~/pages/events/event1"/>
-                <card-additional-content class="event" title="Event 6" content="Saturday 19/06 19:30pm" :imgUrl="'https://cdn.pixabay.com/photo/2016/11/21/16/01/buildings-1846133_1280.jpg'" btnTxt="See More" btnDst="~/pages/events/event1"/>
-                <card-additional-content class="event" title="Event 7" content="Friday 22/06 10AM" :imgUrl="'https://cdn.pixabay.com/photo/2016/11/21/16/01/buildings-1846133_1280.jpg'" btnTxt="See More" btnDst="~/pages/events/event1"/>
-                <card-additional-content class="event" title="Event 8" content="Saturday 19/06 7PM" :imgUrl="'https://cdn.pixabay.com/photo/2016/11/21/16/01/buildings-1846133_1280.jpg'" btnTxt="See More" btnDst="~/pages/events/event1"/>
-                <card-additional-content class="event" title="Event 9" content="Saturday 19/06 19:30pm" :imgUrl="'https://cdn.pixabay.com/photo/2016/11/21/16/01/buildings-1846133_1280.jpg'" btnTxt="See More" btnDst="~/pages/events/event1"/>
-            </div>
-        </div>
-    </div>
+    <transitional-link-cards linkText="Events including this artist" />
+
 
     <footer />
   </section>
 </template>
 
 <script>
-import Footer from '~/components/footer.vue';
 import CardAdditionalContent from '~/components/img-component/card-additional-content.vue';
 import Default from '~/layouts/default.vue';
 import BioHeader from '~/components/artist-bio-header.vue';
+import TransitionalLinkCards from '~/components/transitional-link-cards.vue';
 export default {
     name: "artists",
-    components: {Footer, Default, BioHeader, CardAdditionalContent}
+    components: {Default, BioHeader, CardAdditionalContent, TransitionalLinkCards}
 
 }
 </script>
@@ -132,36 +118,5 @@ section {
     flex-wrap: wrap;
 }
 
-.where {
-    width:100%;
-    display: flex;
-    text-align: center;
-    margin-top: 30px;
-    flex-direction: column;
-}
-
-
-.events{
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    flex-shrink: 1;
-    overflow: scroll;
-    scroll-behavior: smooth;
-    width: 100%;
-    overflow: auto;
-    white-space: nowrap;
-}
-.event {
-    margin:25px;
-}
-
-.scroll-window{
-    width:60%;
-    margin-left: 20%;
-    display: flex;
-
-
-}
 
 </style>
