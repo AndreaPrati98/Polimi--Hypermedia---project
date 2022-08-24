a8c9fa434139832489de578b54e78e68c32200ad
Andrea Prati <a.prati@outlook.it>
2022-08-24 15:58:04 +0200

    created single event page

--- components/new-header.vue
+++ components/new-header.vue
@@ -43,7 +43,7 @@ export default {
             },
             {
             name: 'Events',
-            path: '/',
+            path: '/events',
             },
             {
             name: 'Artists',
---
+++ pages/events/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="section">
+        <title-overlay-image class="header"/>
+        <div class="overview-map-container">
+            <DescriptiveComponent class="overview-comp"/>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4646.175289173596!2d4.812885751804693!3d43.94526359355342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b5ed5580c79235%3A0x292de02a07a25caa!2zVGjDqcOidHJlIExlcyBFdG9pbGVz!5e0!3m2!1sit!2sit!4v1661348281479!5m2!1sit!2sit" 
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="overview-info-container">
+            <info-component :title="'Artists Involved'"/>
+            <descriptive-component/>
+        </div>
+        <div class="all-events">
+
+        </div>
+        <div class="all-events by-type">
+
+        </div>
+    </section>
+</template>
+
+<script>
+import titleOverlayImage from '~/components/title-overlay-image.vue'
+import DescriptiveComponent from '~/components/descriptive-component.vue';
+import InfoComponent from '~/components/information-components/info-component.vue';
+export default {
+    name: "artist-page",
+    components: { titleOverlayImage, DescriptiveComponent, InfoComponent },
+
+}
+</script>
+
+<style scoped>
+
+</style>
