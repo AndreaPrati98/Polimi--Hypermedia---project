eb7fdec2cfe35fea43b5b37c8e0db7fbfdba4689
Andrea Prati <a.prati@outlook.it>
2022-08-25 15:59:57 +0200

    new description component

---
+++ components/information-components/descr-component.vue
@@ -0,0 +1,44 @@
+<template>
+    <section class="description-container">
+        <div class="description">
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Temporibus qui deleniti vero dignissimos consequatur earum officiis ut aut aspernatur omnis 
+            illum quaerat assumenda odit, laborum dicta iure ea saepe. Vel.
+            </p>  
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scope>
+    .description-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    /* width: 100%; */  
+    background-color: var(--palette-red);
+  }
+
+  .description {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 70%;
+    width: 90%;
+    
+    border: 2px solid var(--text-color-light);
+    color: var(--text-color-light);
+
+    margin: 2% 0;
+    padding: 8px 10px;
+    font-size: 30px;
+  }
+</style>
