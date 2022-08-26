bdaedb991cbf9caf95c8273bdd8dc25c68bd7089
Andrea Prati <a.prati@outlook.it>
2022-08-26 10:54:43 +0200

    added a hidden test-page, to test component

---
+++ pages/artists/test-page.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="test-container">
+        <div class="card-container">
+            <card-hover-fix/>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import cardHoverFix from '~/components/img-component/card-hover-fix.vue'
+export default {
+  components: { cardHoverFix },
+    name: 'test-page'
+}
+</script>
+
+<style scoped>
+
+    .test-container {
+        width: 100%;
+        height: 300px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .card-container {
+        height: 300px;
+        width: 300px;
+    }
+
+</style>
