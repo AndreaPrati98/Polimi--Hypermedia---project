3a849bbd407178284c0eaff7f0040a34a4f123da
dmora4 <destiny.mora@hotmail.com>
2022-08-31 17:24:22 +0200

    added vertical bars component

---
+++ components/img-component/vertical-bars.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="vertical-bars-container">
+        <div class="item">
+            <div class="image" style="background-image: url(https://placeimg.com/801/801/nature)"></div>
+            <a href="#section-2"><div class="bar-title"><p>Overview</p></div></a>
+        </div>
+        <div class="item">
+            <div class="image" style="background-image: url(https://placeimg.com/801/801/nature)"></div>
+            <a href="#section-3"><div class="bar-title"><p>Information</p></div></a>
+        </div>
+        <div class="item">
+            <div class="image" style="background-image: url(https://placeimg.com/801/801/nature)"></div>
+            <a href="#section-4"><div class="bar-title"><p>History</p></div></a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "verical-bars",
+
+}
+</script>
+
+<style scoped>
+.bar-button {
+  all:unset;
+  cursor: pointer;
+}
+/* VERTICAL BARS EXAMPLE */
+.vertical-bars-container{
+    display: flex;
+    flex-direction: row;
+    width: 70%;
+    height: 100%;
+}
+
+
+.item {
+  position: relative;
+  width: 33%;
+  height: inherit;
+  min-height: 600px;
+  color: #fff;
+  overflow: hidden;
+  transition: width 0.5s ease;}
+  
+.item:before, .item:after {
+    content: '';
+    position: absolute;
+    left: 0; top: 0;
+    width: 100%; height: 100%;
+  }
+
+.item:after {
+    opacity: 1;
+    transition: opacity 0.5s ease;
+  }
+
+.item:before {
+    background: linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,1) 75%);
+    z-index: 1;
+    opacity: 0;
+    /*transition: opacity 0.5s ease, transform 0.5s ease;*/
+  }
+
+.item:hover {
+    width: 40% !important;
+}
+
+
+.item:hover::before {
+      opacity: 1;
+}
+    
+
+
+.bar-title {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  z-index: 1;
+    font-family: "Oswald";
+    font-size: 1.728rem;
+    line-height: 0;
+}
+
+.image {
+  position: absolute;
+  width: 100%; 
+  height: 100%;
+  top: 0; left: 0;
+  background-size: cover;
+  background-position: center center;
+  transition: filter 0.5s ease;
+  filter: grayscale(100%);
+  overflow: hidden;
+
+}
+
+
+</style>
--- pages/festival.vue
+++ pages/festival.vue
@@ -2,26 +2,7 @@
 <section class="all-sections">
   <!-- SECTION 1 -->
   <div class="section1" id="section-1">
-        <div class="vertical-bars-container">
-            <div class="item">
-                <div class="image" style="background-image: url(https://placeimg.com/801/801/nature)"></div>
-                <a href="#section-2">
-                <div class="bar-title"><p>Overview</p></div>
-                </a>
-            </div>
-            <div class="item">
-                <div class="image" style="background-image: url(https://placeimg.com/801/801/nature)"></div>
-                <a href="#section-3">
-                <div class="bar-title"><p>Information</p></div>
-                </a>
-            </div>
-            <div class="item">
-                <div class="image" style="background-image: url(https://placeimg.com/801/801/nature)"></div>
-                <a href="#section-4">
-                <div class="bar-title"><p>History</p></div>
-                </a>
-            </div>
-        </div>
+        <vertical-bars />
         <div class="heading">
           Festival Information
         </div>
@@ -114,9 +95,10 @@ import Card from '~/components/img-component/card.vue';
 import OverviewComponent from '~/components/information-components/OverviewComponent.vue';
 import MultipleShortOverviewContainer from '~/components/information-components/MultipleShortOverviewContainer.vue';
 import Default from '~/layouts/default.vue';
+import VerticalBars from '~/components/img-component/vertical-bars.vue';
 
 export default {
-    components: { Map, TheHeaderWithImages, OverviewComponent, Card, OverviewComponent, MultipleShortOverviewContainer, Default },
+    components: { Map, TheHeaderWithImages, OverviewComponent, Card, OverviewComponent, MultipleShortOverviewContainer, Default, VerticalBars },
     name: "festival-intro",
     data() {
       const content = {
@@ -149,89 +131,6 @@ export default {
   margin-right: 10px;
   color: var(--palette-blue);
 }
-
-
-
-.bar-button {
-  all:unset;
-  cursor: pointer;
-}
-/* VERTICAL BARS EXAMPLE */
-.vertical-bars-container{
-    display: flex;
-    flex-direction: row;
-    width: 70%;
-    height: 100%;
-}
-
-
-.item {
-  position: relative;
-  width: 33%;
-  height: inherit;
-  min-height: 600px;
-  color: #fff;
-  overflow: hidden;
-  transition: width 0.5s ease;}
-  
-.item:before, .item:after {
-    content: '';
-    position: absolute;
-    left: 0; top: 0;
-    width: 100%; height: 100%;
-  }
-
-.item:after {
-    opacity: 1;
-    transition: opacity 0.5s ease;
-  }
-
-.item:before {
-    background: linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,1) 75%);
-    z-index: 1;
-    opacity: 0;
-    /*transition: opacity 0.5s ease, transform 0.5s ease;*/
-  }
-
-.item:hover {
-    width: 40% !important;
-}
-
-
-.item:hover::before {
-      opacity: 1;
-}
-    
-
-
-.bar-title {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translateX(-50%) translateY(-50%);
-  z-index: 1;
-    font-family: "Oswald";
-    font-size: 1.728rem;
-    line-height: 0;
-}
-
-.image {
-  position: absolute;
-  width: 100%; 
-  height: 100%;
-  top: 0; left: 0;
-  background-size: cover;
-  background-position: center center;
-  transition: filter 0.5s ease;
-  filter: grayscale(100%);
-  overflow: hidden;
-
-}
-
-
-
-/* END OF MOVING VERTICAL BARS EXAMPLE */
-
 
 
 
