f5f4e2e5896793d6704786ce4c3c15af8ca0e03a
Andrea Prati <a.prati@outlook.it>
2022-09-09 11:46:03 +0200

    indenting the file

--- pages/places/index.vue
+++ pages/places/index.vue
@@ -1,51 +1,55 @@
 <template>
-    <section class="all-sections">
-        <the-header-with-title class="header"
-            :title="pageData.title" 
-            :subtitle="pageData.shortDescription" 
-            :imgUrl="pageData.imgUrl" />
-        <breadcrumbs-component/>
-        <subheader-component class="subheader"
-            :content="pageData.description" />
-        <grid-component class="grid-container" 
-            :partialPath="'places'"
-            :objList="allPlaces"/>
-    </section>
+  <section class="all-sections">
+    <the-header-with-title
+      class="header"
+      :title="pageData.title"
+      :subtitle="pageData.shortDescription"
+      :imgUrl="pageData.imgUrl"
+    />
+    <breadcrumbs-component />
+    <subheader-component class="subheader" :content="pageData.description" />
+    <grid-component
+      class="grid-container"
+      :partialPath="'places'"
+      :objList="allPlaces"
+    />
+  </section>
 </template>
 
 <script>
-import TheHeaderWithTitle from '~/components/headers/TheHeaderWithTitle.vue'
-import SubheaderComponent from '~/components/information-components/SubheaderComponent.vue'
-import GridComponent from '~/components/medium-components/GridComponent.vue'
-import BreadcrumbsComponent from '~/components/utilities-components/BreadcrumbsComponent.vue'
+import TheHeaderWithTitle from "~/components/headers/TheHeaderWithTitle.vue";
+import SubheaderComponent from "~/components/information-components/SubheaderComponent.vue";
+import GridComponent from "~/components/medium-components/GridComponent.vue";
+import BreadcrumbsComponent from "~/components/utilities-components/BreadcrumbsComponent.vue";
 
 export default {
-    name: 'places-page',
-    components: {
+  name: "places-page",
+  components: {
     TheHeaderWithTitle,
     SubheaderComponent,
     GridComponent,
-    BreadcrumbsComponent
-},
-    data() {
-        const pageData = {
-            title: "All the Places",
-            shortDescription: "Getting fascinated by Avignon is easy. The places are all amazing.",
-            description: "During the Festival, the city of Avignon comes alive. Historical landmarks that are usually dormant and recognized for what they once were, get to breathe in the life of art once again. In true spirit of the circle of life, the expansion of the Festival has also enabled to fund the construction of new venues and places to host events. Spectators not only get to experience the show they are attending, but they also get add to the significance of each place that helps bring the Festival to life.",
-            imgUrl: "https://cdn.pixabay.com/photo/2016/03/10/17/01/stage-1248769__480.jpg",
-        }
+    BreadcrumbsComponent,
+  },
+  data() {
+    const pageData = {
+      title: "All the Places",
+      shortDescription:
+        "Getting fascinated by Avignon is easy. The places are all amazing.",
+      description:
+        "During the Festival, the city of Avignon comes alive. Historical landmarks that are usually dormant and recognized for what they once were, get to breathe in the life of art once again. In true spirit of the circle of life, the expansion of the Festival has also enabled to fund the construction of new venues and places to host events. Spectators not only get to experience the show they are attending, but they also get add to the significance of each place that helps bring the Festival to life.",
+      imgUrl:
+        "https://cdn.pixabay.com/photo/2016/03/10/17/01/stage-1248769__480.jpg",
+    };
 
-        return {
-            pageData
-        }
-    },
-    async asyncData({ $axios }) {
-        
-        const { data } = await $axios.get('/api/places')
-        return {
-            allPlaces: data,
-        }
-    },
-
-}
+    return {
+      pageData,
+    };
+  },
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get("/api/places");
+    return {
+      allPlaces: data,
+    };
+  },
+};
 </script>
